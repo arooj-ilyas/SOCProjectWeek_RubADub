83eb814d55eb6d39e9e9085ae416fb744158ff61
arooj-ilyas <112420017+arooj-ilyas@users.noreply.github.com>
2022-11-24 20:15:31 +0000

    css names

--- src/Components/Menu/Menu.js
+++ src/Components/Menu/Menu.js
@@ -7,9 +7,9 @@ function Menu() {
 	return (
 		<div className='menu'>
 			{/* <b id='menu'>Menu</b> */}
-			<div class='bar1'></div>
-			<div class='bar2'></div>
-			<div class='bar3'></div>
+			<div className='bar1'></div>
+			<div className='bar2'></div>
+			<div className='bar3'></div>
 		</div>
 	);
 }
--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -3,15 +3,16 @@
 //To contain: input field with search icon
 //Props: ??
 
-function SearchBar({handleChange}) {
-    return(
-        <input type="text"
-        placeholder="🔍 search by key word or phrase"
-        className="search-bar"
-        id="search-bar"
-        onChange = {handleChange}>
-    </input>
-    )
-  }
-  
-  export default SearchBar;
+function SearchBar({ handleChange }) {
+	return (
+		<input
+			type='text'
+			placeholder='🔍 search by programming language, location or skills...'
+			className='search-bar'
+			id='search-bar'
+			onChange={handleChange}
+		></input>
+	);
+}
+
+export default SearchBar;
