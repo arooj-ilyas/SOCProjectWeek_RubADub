e053bd3817c72bbd4474daddc5289995c56b3716
soph-th <109481663+soph-th@users.noreply.github.com>
2022-11-24 00:40:14 +0000

    dropdown values imported from database call, hit limit so need to look at elephant sql again

--- src/Components/AdvancedFilter/AdvancedFilter.js
+++ src/Components/AdvancedFilter/AdvancedFilter.js
@@ -3,15 +3,10 @@
 //To contain: Dropdown
 //Props: ??
 
+import { useEffect, useState } from "react";
 import Dropdown from "../Dropdown/Dropdown";
 
-//function to get pLang table data
-  async function getTableData() {
-    const response = await fetch('http://localhost:3000/tables/pLangs', {method: 'GET', headers: {accept: 'application/JSON'},})
-    const dataJson = await response.json() 
-    const pLangData =(dataJson.payload)
-  }
-  getTableData()
+
 
 
 const pLang = [
@@ -36,17 +31,49 @@ const pLang = [
   ]
 
 function AdvancedFilter({onChangeProgrammingLang, onChangeLocation, onChangeSpokenLang}) {
+
+const [programmingLanguageData, setProgrammingLanguageData] = useState({})
+const [spokenLanguageData, setSpokenLanguageData] = useState({})
+const [locationData, setLocationData] = useState({})
+
+//function to get pLang table data
+
+useEffect(()=>{
+  async function getTableData() {
+    const response = await fetch('http://localhost:3000/tables/pLangs', {method: 'GET', headers: {accept: 'application/JSON'},})
+    const dataJson = await response.json() 
+    setProgrammingLanguageData(dataJson.payload)
+  }
+  getTableData()}, [])
+
+useEffect(()=>{
+    async function getTableData() {
+      const response = await fetch('http://localhost:3000/tables/sLangs', {method: 'GET', headers: {accept: 'application/JSON'},})
+      const dataJson = await response.json() 
+      setSpokenLanguageData(dataJson.payload)
+    }
+    getTableData()}, [])
+
+  useEffect(()=>{
+      async function getTableData() {
+        const response = await fetch('http://localhost:3000/tables/locations', {method: 'GET', headers: {accept: 'application/JSON'},})
+        const dataJson = await response.json() 
+        setLocationData(dataJson.payload)
+      }
+      getTableData()}, [])
+
+
     return(
         <div className="advanced-filter" id="advanced-filter">
-            <Dropdown options={pLang}
+            <Dropdown options={programmingLanguageData}
             onChange={onChangeProgrammingLang}
             placeholder="Programming language"/>
 
-            <Dropdown options={location}
+            <Dropdown options={locationData}
             onChange={onChangeLocation}
             placeholder="Location"/>
 
-            <Dropdown options={sLang}
+            <Dropdown options={spokenLanguageData}
             onChange={onChangeSpokenLang}
             placeholder="Spoken language"/>
         </div>
