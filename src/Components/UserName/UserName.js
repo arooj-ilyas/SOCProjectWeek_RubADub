82c5bb8ca5710b265f1a97ff3ff7dfcc511e2696
arooj-ilyas <112420017+arooj-ilyas@users.noreply.github.com>
2022-11-24 23:23:28 +0000

    user cards matching wireframe

--- src/Components/UserName/UserName.js
+++ src/Components/UserName/UserName.js
@@ -3,12 +3,23 @@
 //To contain: name of user
 //Props: ??
 
-function UserName({name}) {
-    //console.log(data.name)
-    return(
-        <b className="user-name" id="user-name">{name}</b>
-    )
-  }
-  
-  export default UserName;
-   
+function UserName({ name }) {
+	//console.log(data.name)
+	return (
+		<>
+			<div className='nameandflag'>
+				<img
+					className='flag-image'
+					id='user-image'
+					src='https://cdn-icons-png.flaticon.com/512/197/197374.png'
+					alt='duck'
+				></img>
+				<b className='user-name' id='user-name'>
+					{name}
+				</b>
+			</div>
+		</>
+	);
+}
+
+export default UserName;
