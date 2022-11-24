f1ed445c6e9d40d3fc98cca3bda8af82713962e8
soph-th <109481663+soph-th@users.noreply.github.com>
2022-11-24 12:32:50 +0000

    filtering working

--- src/Components/AdvancedFilter/AdvancedFilter.js
+++ src/Components/AdvancedFilter/AdvancedFilter.js
@@ -6,30 +6,6 @@
 import { useEffect, useState } from "react";
 import Dropdown from "../Dropdown/Dropdown";
 
-
-
-
-const pLang = [
-    { value: 'c++', label: 'C++' },
-    { value: 'javascript', label: 'JavaScript' },
-    { value: 'python', label: 'Python' }
-    //need to import actual country/lang etc options here
-  ]
-
-  const location = [
-    { value: 'uk', label: 'UK' },
-    { value: 'france', label: 'France' },
-    { value: 'hawaii', label: 'Hawaii' }
-    //need to import actual country/lang etc options here
-  ]
-
-  const sLang = [
-    { value: 'english', label: 'English' },
-    { value: 'french', label: 'French' },
-    { value: 'japanese', label: 'Japanese' }
-    //need to import actual country/lang etc options here
-  ]
-
 function AdvancedFilter({onChangeProgrammingLang, onChangeLocation, onChangeSpokenLang}) {
 
 const [programmingLanguageData, setProgrammingLanguageData] = useState({})
