61d039460bf52fb6166339c2fcb0b8ae8c6a6244
arooj-ilyas <112420017+arooj-ilyas@users.noreply.github.com>
2022-11-30 14:42:38 +0000

    cypress tests 3/4 complete

--- src/Components/Dropdown/Dropdown.js
+++ src/Components/Dropdown/Dropdown.js
@@ -3,23 +3,24 @@
 //To contain: mapped results of option for dropdown category
 //Props: ??
 
-import React from 'react'
-import Select from 'react-select'
+import React from "react";
+import Select from "react-select";
 
 /**
  * Creates individual dropdown Select element, inheriting options, placeholder and onChange props. Select component is from React-Select library.
  */
-function Dropdown({options, placeholder, onChange}) {
-    return(
-        <div className="dropdown" id="dropdown">
-            <Select
-            options={options}
-            placeholder ={placeholder}
-            name={placeholder}
-            onChange={onChange}
-            />
-        </div>
-    )
-  }
-  
-  export default Dropdown;
+function Dropdown({ options, dropdownName, onChange, dropdownId }) {
+	return (
+		<div className='dropdown' id='dropdown'>
+			<label htmlFor={dropdownId}>{dropdownName}</label>
+			<Select
+				id={dropdownId}
+				options={options}
+				name={dropdownName}
+				onChange={onChange}
+			/>
+		</div>
+	);
+}
+
+export default Dropdown;
